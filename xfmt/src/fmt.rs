f04f7b23c0412f9d791be2da7e66f691e73ce485
Turtel216 <papadimitrios216@gmail.com>
2024-09-15 06:41:13 +0300

    Added Generator struct

--- xfmt/src/fmt.rs
+++ xfmt/src/fmt.rs
@@ -4,6 +4,8 @@
 //
 // The formatter interface used by the cli tool
 
+use std::collections::HashSet;
+
 // A node in the AST
 enum Node {
     Text(String),
@@ -12,20 +14,79 @@ enum Node {
     Indent(Vec<Node>), // Renders on eore more nodes
     Group(NodeGroup), // A grouping of nodes
     Nodes(Vec<Node>), // A collection of nodes that are render without spacial handling
+} //TODO Add IfWrap
+
+enum Wrap {
+    Enable,
+    Detect,
+}
+
+impl Wrap {
+    fn enable(&self) -> bool {
+        match self {
+            Wrap::Enable => true,
+            _ => false,
+        }
+    }
+}
+
+impl Node {
+    fn width(&mut self, wrapped: &[isize]) -> isize {
+        match self {
+            Node::Nodes(nodes) => nodes.iter_mut().map(|n| n.width(wrapped)).sum(),
+            Node::Group(node_group) => node_group.nodes.iter_mut().map(|n| n.width(wrapped)).sum(),
+            Node::Indent(nodes) => nodes.iter_mut().map(|n| n.width(wrapped)).sum(),
+            Node::Text(source) => source.len() as isize,
+            Node::SpaceOrLine => 1,
+            _ => 0,
+        }
+    }
 }
 
 // A grouping of nodes
 struct NodeGroup {
     nodes: Vec<Node>, // The nodes inside the group
-    id: usize,        // The id of the group
+    id: isize,        // The id of the group
 }
 
-pub struct fmt {
-    id_counter: usize, // Variable for keeping track of the next node
+// Struct holding formatter meta data
+pub struct Generator {
+    buffer: String,          // Holds formatted code
+    size: usize,             // Number of characters on current line
+    indent: usize,           // indentation size
+    indent_size: usize,      // Size of an indentation
+    max: usize,              // Max numbers per line
+    wrapped: HashSet<isize>, // Hash set with all groups that need to be wrapped
 }
 
-impl fmt {
-    pub fn new() -> Self {
-        Self { id_counter: 0 }
+// Indentation template of 3 spaces
+const INDENT: &str = "   ";
+
+impl Generator {
+    pub fn new(max: usize) -> Self {
+        Self {
+            buffer: String::new(),
+            size: 0,
+            indent: 0,
+            indent_size: 3,
+            max,
+            wrapped: HashSet::new(),
+        }
     }
+
+    fn text(&mut self, value: String, char_num: usize) -> () {
+        self.size += char_num;
+        self.buffer.push_str(&value);
+    }
+
+    fn new_line(&mut self) -> () {
+        self.size = self.indent_size * self.indent;
+        self.buffer.push('\n');
+
+        for _ in 0..self.indent {
+            self.buffer.push_str(INDENT);
+        }
+    }
+
+    fn node(&mut self, node: Node) -> () {}
 }
