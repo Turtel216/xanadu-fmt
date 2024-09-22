e975bc06b55e6121aa3a4426cfd9f3912db05706
Turtel216 <papadimitrios216@gmail.com>
2024-09-22 18:27:47 +0300

    Added format_file function

--- xfmt/src/lib.rs
+++ xfmt/src/lib.rs
@@ -2,6 +2,46 @@
 // Use of this source code is governed by a MIT
 // license that can be found in the LICENSE file.
 
+use std::{
+    fs::{self, File},
+    io::Write,
+    process,
+};
+
+use fmt::Formatter;
+
 mod builder;
 pub mod fmt;
 mod tokenizer;
+
+pub fn format_file(path: &String) -> () {
+    let code = match fs::read_to_string(path) {
+        Ok(content) => content,
+        Err(error) => {
+            eprint!("Unable to read file {}: {}", path, error);
+            process::exit(74);
+        }
+    };
+
+    let mut formatter = Formatter::new(&code);
+    let output = formatter.format();
+    write_to_file(path, &output);
+}
+
+fn write_to_file(path: &String, code: &String) -> () {
+    let mut file = match File::open(path) {
+        Ok(s) => s,
+        Err(e) => {
+            eprint!("Unable to write to file {}: {}", path, e);
+            process::exit(74);
+        }
+    };
+
+    match file.write_all(code.as_bytes()) {
+        Ok(_) => (),
+        Err(e) => {
+            eprint!("Unable to read file {}: {}", path, e);
+            process::exit(74);
+        }
+    }
+}
--- xfmt/src/main.rs
+++ xfmt/src/main.rs
@@ -2,6 +2,18 @@
 // Use of this source code is governed by a MIT
 // license that can be found in the LICENSE file.
 
+use std::env;
+
+use xfmt::format_file;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() == 1 {
+        println!("Eror");
+    } else if args.len() == 2 {
+        format_file(&args[1]);
+    } else {
+        println!("Usage: xfmt [path]");
+    }
 }
